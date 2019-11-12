f6df546fd49d6e6a8d1fbdda2e25c9e1bd239726
rosskouk <rosskouk@gmail.com>
2019-11-12 13:28:58 +0000

    - Added new version of setup.py from Alerta Contrib Official Repo

--- routing/routing.py
+++ routing/routing.py
@@ -1,3 +1,13 @@
+# Example plugin routing rules file
+
+
+#def rules(alert, plugins):
+
+#    if alert.severity == 'debug':
+#        return []
+#    else:
+#        return [plugins['reject']]
+
 def rules(alert, plugins):
 
     print(plugins)
--- routing/setup.py
+++ routing/setup.py
@@ -1,23 +1,27 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+
+import setuptools
 
 version = '0.0.1'
 
-setup(
-    name="alerta-routing",
+setuptools.setup(
+    name='alerta-routing',
     version=version,
     description='Alerta routing rules for plugins',
-    url='https://github.com/rosskouk/docker-image-alerta',
+    url='https://github.com/alerta/alerta-contrib',
     license='Apache License 2.0',
-    author='Ross Stewart',
-    author_email='rosskouk@gmail.com',
-    packages=find_packages(),
+    author='Nick Satterly',
+    author_email='nick.satterly@theguardian.com',
     py_modules=['routing'],
-    install_requires=[],
+    install_requires=[
+        'requests',
+        'alerta-server'
+    ],
     include_package_data=True,
-    zip_safe=True,
+    zip_safe=False,
     entry_points={
         'alerta.routing': [
-          'rules = routing:rules'
+            'rules = routing:rules'
         ]
     }
 )
