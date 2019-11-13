6145f821b7ff0877fa9cfc9a79353a1b91601542
rosskouk <rosskouk@gmail.com>
2019-11-13 17:00:34 +0000

    - Added debug statements to routing rules

--- routing/routing.py
+++ routing/routing.py
@@ -3,9 +3,19 @@
 # Ensure alerts have occurred at least 3 time and are not informational before alerting
 def rules(alert, plugins):
 
-    if alert.duplicate_count <= 2:
-        return [plugins['reject']]
-    elif alert.severity not in ['informational']:
-        return [plugins['reject'], plugins['telegram']]
+    print('\n\nAlert Dup Count: ' + str(alert.duplicate_count))
+    print('\n\nAlert Severity: ' + str(alert.severity))
+    if alert.duplicate_count:
+        print('\n\nAlert Dup Count IF: ' + str(alert.duplicate_count))
+        if alert.duplicate_count <= 2:
+            print('\n\nAlert duplicate count was less than 2 - reject')
+            return [plugins['reject']]
+        elif alert.severity not in ['informational']:
+            print('\n\nAlert severity was not informational - reject, telegram')
+            return [plugins['reject'], plugins['telegram']]
+        else:
+            print('\n\nGot to first else - reject')
+            return [plugins['reject']]
     else:
+        print('\n\nGot to second else - reject')
         return [plugins['reject']]
