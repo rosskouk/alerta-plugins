ccac13beb3f26ea5ddb562668f9604f88da11552
rosskouk <rosskouk@gmail.com>
2019-11-13 18:22:26 +0000

    - Rewrote routing rules as duplicate_count is never set.

--- routing/routing.py
+++ routing/routing.py
@@ -1,24 +1,16 @@
 # Alerta plugin routing rules
 
-# Ensure alerts have occurred at least 3 time and are not informational before alerting
+# Only send warning and critical alerts via Telegram.
 def rules(alert, plugins):
 
-    print('Alert attributes: \n\n')
-    print(dir(alert))
-    print('\n\n')
-    print('\n\nAlert Dup Count: ' + str(alert.duplicate_count))
-    print('\n\nAlert Severity: ' + str(alert.severity))
-    if alert.duplicate_count:
-        print('\n\nAlert Dup Count IF: ' + str(alert.duplicate_count))
-        if alert.duplicate_count <= 2:
-            print('\n\nAlert duplicate count was less than 2 - reject')
-            return [plugins['reject']]
-        elif alert.severity not in ['informational']:
-            print('\n\nAlert severity was not informational - reject, telegram')
-            return [plugins['reject'], plugins['telegram']]
-        else:
-            print('\n\nGot to first else - reject')
-            return [plugins['reject']]
+    #print('Available attributes in alert object: \n\n')
+    #print(dir(alert))
+    #print('\n\n')
+    #print('\n\nAlert Dup Count: ' + str(alert.duplicate_count))
+    #print('\n\nAlert Severity: ' + str(alert.severity))
+    if alert.severity not in ['informational']:
+        print('\n\nAlert severity was not informational - reject, telegram')
+        return [plugins['reject'], plugins['telegram']]
     else:
-        print('\n\nGot to second else - reject')
+        print('\n\nAlert severity was informational - reject')
         return [plugins['reject']]
